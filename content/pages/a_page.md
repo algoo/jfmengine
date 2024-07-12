0866cd93181f24d2529830d21c4ed8a9e5cffa4a
Clément <clement.becquet@algoo.fr>
2024-07-12 10:19:56 +0200

    add django-jinja-markdown extension

--- content/pages/a_page.md
+++ content/pages/a_page.md
@@ -8,8 +8,25 @@ template_engine jinja2
 }
 
 ---
+{% from "widgets/page_header.html" import page_header %}
+
 
 <h2>Page 1</h2>
 
 <p>Go to <a href="{{ url('page', args=['page2']) }}">page 2</a> </p>
 
+<article>
+{% markdown %}
+```
+This is markdown.
+```
+{% endmarkdown %}
+</article>
+
+
+{{ page_header(markdown('
+test
+#test
+####test
+
+')) }}
--- jssg/jinja2.py
+++ jssg/jinja2.py
@@ -1,6 +1,6 @@
 from django.templatetags.static import static
 from django.urls import reverse
-
+from django_jinja_markdown.templatetags.md import markdown
 from jinja2 import Environment
 
 from jssg.templatetags.filter_opengraph_metadata import filter_opengraph_metadata
@@ -14,6 +14,7 @@ def environment(**options):
         {
             "static": static,
             "url": reverse,
+            "markdown": markdown
         }
     )
     env.filters.update(
