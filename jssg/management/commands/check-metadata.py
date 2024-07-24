c186f93b1dbd60b78e85e671a63d66426b240b01
Clément <clement.becquet@algoo.fr>
2024-07-24 09:49:56 +0200

    Improve lisibility and add a warning if JFME_CONTENT_REQUIRED_METADATA setting is empty

--- jssg/management/commands/check-metadata.py
+++ jssg/management/commands/check-metadata.py
@@ -5,19 +5,21 @@ from pathlib import Path
 
 class MetadataStatus :
     
-    def get_metadata_status_for(self, page) :
-        self.missing = []
-        self.empty = []
+    def get_metadata_status_for(page) :
+        metadata_status = MetadataStatus()
+        metadata_status.missing = []
+        metadata_status.empty = []
         for required_metadata in settings.JFME_CONTENT_REQUIRED_METADATA :
             if required_metadata not in page.metadata :
-                self.missing.append(required_metadata)
+                metadata_status.missing.append(required_metadata)
             elif page.metadata[required_metadata] == "" :
-                self.empty.append(required_metadata)
-        self.complete = self.missing == [] and self.empty == []
+                metadata_status.empty.append(required_metadata)
+        metadata_status.complete = (metadata_status.missing == []) and (metadata_status.empty == [])
         if len(settings.JFME_CONTENT_REQUIRED_METADATA) > 0 :
-            self.progression = (len(settings.JFME_CONTENT_REQUIRED_METADATA) - len(self.missing) - len(self.empty)) * 100 / len(settings.JFME_CONTENT_REQUIRED_METADATA)
+            metadata_status.progression = (len(settings.JFME_CONTENT_REQUIRED_METADATA) - len(metadata_status.missing) - len(metadata_status.empty)) * 100 / len(settings.JFME_CONTENT_REQUIRED_METADATA)
         else :
-            self.progression = 100
+            metadata_status.progression = 100
+        return metadata_status
 
 class Command(BaseCommand):
     help = "Check if metadata in JFME_CONTENT_REQUIRED_METADATA setting are specified in pages."
@@ -37,17 +39,22 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options) :
+
+        if settings.JFME_CONTENT_REQUIRED_METADATA == [] :
+            self.stdout.write(self.style.WARNING(
+                "Warning : no metadata specified in JFME_CONTENT_REQUIRED_METADATA setting."
+            ))
+
         for page in Page.load_glob(path = list(map(lambda p : Path(p).absolute(), options["content path"])), all = True) :
 
-            metadata_status = MetadataStatus()
-            metadata_status.get_metadata_status_for(page)
-            
+            metadata_status = MetadataStatus.get_metadata_status_for(page)
+
             self.stdout.write("{:3.0f}% : {}".format(
                 metadata_status.progression,
                 page.path.relative_to(page.content_page_dir))
             )
 
-            if options["verbosity"]>1 or options["verbose"] :
+            if options["verbosity"] > 1 or options["verbose"] :
                if not metadata_status.complete :
                     for missing in metadata_status.missing :
                        self.stdout.write("\t- '%s' is missing" % missing)
