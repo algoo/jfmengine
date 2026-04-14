9142f04dbcb0df77c1dc6dd5e51255470f1dc9fb
damien <damien.accorsi@algoo.fr>
2026-04-14 11:27:02 +0200

    Allow to define static pages (text-based or binary) that would be served raw from the main directory

--- jssg/urls.py
+++ jssg/urls.py
@@ -21,6 +21,7 @@ from jssg import settings
 
 from django.contrib.sitemaps.views import sitemap
 from jssg.sitemaps import PageSitemap, PostSitemap, PostListSitemap, ConstantUrlSitemap
+from jssg.views import StaticPageView
 
 urlpatterns = [
     distill_path(
@@ -53,6 +54,13 @@ urlpatterns = [
         name="django.contrib.sitemaps.views.sitemap",
     ),
 ]
+
+for spf in settings.JFME_STATIC_PAGES:  # spf = static page filepath
+    urlpatterns.append(
+        distill_path(spf, StaticPageView.as_view(static_page_filepath=spf), name=spf),
+    )
+
+
 
 if len(list(Post.get_posts())) > 0:
     urlpatterns += [
--- jssg/views.py
+++ jssg/views.py
@@ -12,22 +12,25 @@
 #
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see <https://www.gnu.org/licenses/>.
+import mimetypes
+import os
 from pathlib import Path
 from typing import List
 
 from django.conf import settings
 from django.contrib.syndication.views import Feed
 from django.db.models.base import Model as Model
+from django.http import Http404,HttpResponse
 from django.shortcuts import render
 from django.urls import reverse
 from django.utils.feedgenerator import Atom1Feed
-from django.views.generic import DetailView
+from django.views.generic import DetailView, View
 
 from jssg.models import Page, Post, PostList
 
 
 class PostFeedsView(Feed):
-    title = "MY WEBSITE - last articles"
+    title = "Last articles"
     link = ""
     feed_type = Atom1Feed
 
@@ -89,6 +92,46 @@ class PostListView(DetailView):
         return PostList.load_post_list_with_category(
             self.kwargs["category"], self.kwargs["page"]
         )
+
+class StaticPageView(View):
+    """
+    Rendering of static pages. Static pages are raw pages, like a pdf or text file.
+    Compared to static files, they can be found in the original page tree, eg /robots.txt or /en/documentation.pdf.
+    If it was static files, they would be found in /static/robots.txt or /static/en/documentation.pdf, etc
+    """
+    static_page_filepath: str = None  # set via as_view(filename="...")
+
+    def get(self, request):
+        if not self.static_page_filepath:
+            raise Http404("Wrong usage of FileContentView.")
+
+        file_path = ""
+        for folder_path in settings.JFME_PAGES_DIRS:
+            static_page_absolute_path = os.path.join(folder_path, self.static_page_filepath)
+            if os.path.exists(static_page_absolute_path):
+                file_path = static_page_absolute_path
+                print(f"Found {file_path}")
+                break
+
+        if not file_path:
+            print(f"{self.static_page_filepath} not found in {settings.JFME_PAGES_DIRS}")
+            raise Http404("File not found.")
+
+        mime_type, encoding = mimetypes.guess_type(self.static_page_filepath)
+        content_type = mime_type or "application/octet-stream"
+        if encoding:
+            content_type += f"; charset={encoding}"
+
+        print(f"ENCODING IS {encoding}")
+        is_binary = not (mime_type and mime_type.startswith("text/"))
+        open_kwargs = {} if is_binary else {"encoding": "utf-8"}
+
+        content = ""
+        with open(file_path, "rb" if is_binary else "r", **open_kwargs) as f:
+            content = f.read()
+
+        return HttpResponse(content, content_type=content_type)
+
 
 def jfme_seo_helper(request):
 
